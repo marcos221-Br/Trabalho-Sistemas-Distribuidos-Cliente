ac736c2905a39b324364fc4571845d290b51d92e
Marcos Gomes <marcosartemio221@gmail.com>
2024-06-03 21:19:31 -0300

    CRUD Vaga
    Added CRUD for Vaga

--- src/main/java/com/example/trabalhosistemasdistribuidos/CadastrarVagaController.java
+++ src/main/java/com/example/trabalhosistemasdistribuidos/CadastrarVagaController.java
@@ -3,8 +3,13 @@ package com.example.trabalhosistemasdistribuidos;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import com.example.trabalhosistemasdistribuidos.enums.CompetenciaEnum;
 import com.example.trabalhosistemasdistribuidos.modelo.Competencia;
+import com.example.trabalhosistemasdistribuidos.modelo.Login;
+import com.example.trabalhosistemasdistribuidos.modelo.Vaga;
 import com.example.trabalhosistemasdistribuidos.modelo.VagaId;
 
 import javafx.collections.FXCollections;
@@ -13,16 +18,21 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.paint.Color;
 
 public class CadastrarVagaController {
 
     private ObservableList<String> listaCompetencias;
     private ObservableList<String> listaEstados;
     private ObservableList<Competencia> competencias;
+    private ObservableList<VagaId> listaVagas;
+    private Vaga vaga;
+    private ToJson json;
 
     @FXML
     private ResourceBundle resources;
@@ -85,16 +95,28 @@ public class CadastrarVagaController {
     private Button btnCarregar;
 
     @FXML
-    private Button btnVisualizar;
-    
-    @FXML
-    void visualizar(ActionEvent event) {
-
-    }
+    private Label mensagem;
 
     @FXML
     void carregar(ActionEvent event) {
-
+        desativarMensagem();
+        idVaga.clear();
+        nomeVaga.clear();
+        descricaoVaga.clear();
+        faixaSalarialVaga.clear();
+        listaVagas.clear();
+        competencias.clear();
+        tabela.setItems(competencias);
+        String jsonRecebido;
+        String[] funcoes = {"email","token"};
+        String[] valores = {Login.getLogin(),Login.getToken()};
+        json = new ToJson("listarVagas", funcoes, valores);
+        jsonRecebido = ClientApplication.enviarSocket(json.getJson());
+        json.setJson(new JSONObject(jsonRecebido));
+        for(int i = 0; i<((JSONArray) json.getFuncao("vagas")).length(); i++){
+            listaVagas.add(new VagaId(((JSONArray) json.getFuncao("vagas")).getJSONObject(i).getString("nomeVaga"),((JSONArray) json.getFuncao("vagas")).getJSONObject(i).getInt("idVaga")));
+        }
+        tabelaVagas.setItems(listaVagas);
     }
 
     @FXML
@@ -110,22 +132,75 @@ public class CadastrarVagaController {
 
     @FXML
     void alterar(ActionEvent event) {
-
+        desativarMensagem();
+        vaga = new Vaga(Integer.parseInt(idVaga.getText()),nomeVaga.getText(),Float.parseFloat(faixaSalarialVaga.getText()),descricaoVaga.getText(),estadoVaga.getSelectionModel().getSelectedItem());
+        for (Competencia competencia : tabela.getItems()) {
+            vaga.setCompetencia(competencia.getCompetencia());
+        }
+        if(vaga.atualizar()){
+            novaMensagem("Vaga atualizada com sucesso!", Color.GREEN);
+        }else{
+            novaMensagem("Impossível atualizar vaga!", Color.RED);
+        }
     }
 
     @FXML
     void buscar(ActionEvent event) {
-
+        desativarMensagem();
+        competencias.clear();
+        vaga = new Vaga(tabelaVagas.getSelectionModel().getSelectedItem().getIdVaga());
+        vaga.buscar();
+        nomeVaga.setText(tabelaVagas.getSelectionModel().getSelectedItem().getNomeVaga());
+        idVaga.setText(tabelaVagas.getSelectionModel().getSelectedItem().getIdVaga()+"");
+        faixaSalarialVaga.setText(vaga.getFaixaSalarial()+"");
+        descricaoVaga.setText(vaga.getDescricao());
+        estadoVaga.setValue(vaga.getEstado());
+        for (String competencia : vaga.getCompetencia()) {
+            competencias.add(new Competencia(competencia));
+        }
+        tabela.setItems(competencias);
     }
 
     @FXML
     void cadastrar(ActionEvent event) {
-
+        desativarMensagem();
+        vaga = new Vaga(nomeVaga.getText(),Float.parseFloat(faixaSalarialVaga.getText()),descricaoVaga.getText(),estadoVaga.getSelectionModel().getSelectedItem());
+        for (Competencia competencia : tabela.getItems()) {
+            vaga.setCompetencia(competencia.getCompetencia());
+        }
+        if(vaga.cadastrar()){
+            novaMensagem("Vaga cadastrada com sucesso!", Color.GREEN);
+        }else{
+            novaMensagem("Impossível cadastrar vaga!", Color.RED);
+        }
     }
 
     @FXML
     void excluir(ActionEvent event) {
+        desativarMensagem();
+        vaga = new Vaga(Integer.parseInt(idVaga.getText()));
+        if(vaga.excluir()){
+            novaMensagem("Vaga excluida com sucesso!", Color.GREEN);
+            nomeVaga.clear();
+            idVaga.clear();
+            faixaSalarialVaga.clear();
+            descricaoVaga.clear();
+            competencias.clear();
+            tabela.setItems(competencias);
+        }else{
+            novaMensagem("Impossível excluir a vaga!", Color.RED);
+        }
+    }
 
+    private void desativarMensagem(){
+        mensagem.setVisible(false);
+        mensagem.textFillProperty().set(Color.RED);
+    }
+
+    private void novaMensagem(String text, Color color){
+        mensagem.setText(text);
+        mensagem.textFillProperty().set(color);
+        mensagem.setVisible(true);
     }
 
     @FXML
@@ -142,6 +217,10 @@ public class CadastrarVagaController {
         competencias = FXCollections.observableArrayList();
 
         columnCompetencia.setCellValueFactory(new PropertyValueFactory<>("competencia"));
+        
+        listaVagas = FXCollections.observableArrayList();
+        columnId.setCellValueFactory(new PropertyValueFactory<>("idVaga"));
+        columnNome.setCellValueFactory(new PropertyValueFactory<>("nomeVaga"));
     }
 
 }
--- src/main/java/com/example/trabalhosistemasdistribuidos/modelo/Vaga.java
+++ src/main/java/com/example/trabalhosistemasdistribuidos/modelo/Vaga.java
@@ -1,10 +1,159 @@
 package com.example.trabalhosistemasdistribuidos.modelo;
 
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.example.trabalhosistemasdistribuidos.ClientApplication;
+import com.example.trabalhosistemasdistribuidos.ToJson;
+
 public class Vaga {
     private int codigo;
-    private String ramo;
+    private String nome;
     private float faixaSalarial;
-    private String Competencia;
-    private String Experiencia;
     private String descricao;
+    private ArrayList<String> competencia;
+    private String estado;
+    private ToJson json;
+
+    public Vaga(String nome, float faixaSalarial, String descricao, String estado){
+        this.nome = nome;
+        this.faixaSalarial = faixaSalarial;
+        this.descricao = descricao;
+        this.estado = estado;
+        competencia = new ArrayList<>();
+    }
+
+    public Vaga(int codigo, String nome, float faixaSalarial, String descricao, String estado){
+        this.codigo = codigo;
+        this.nome = nome;
+        this.faixaSalarial = faixaSalarial;
+        this.descricao = descricao;
+        this.estado = estado;
+        competencia = new ArrayList<>();
+    }
+
+    public Vaga(int codigo){
+        this.codigo = codigo;
+        competencia = new ArrayList<>();
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getnome() {
+        return nome;
+    }
+
+    public void setnome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getFaixaSalarial() {
+        return faixaSalarial;
+    }
+
+    public void setFaixaSalarial(float faixaSalarial) {
+        this.faixaSalarial = faixaSalarial;
+    }
+
+    public ArrayList<String> getCompetencia() {
+        return competencia;
+    }
+
+    public void setCompetencia(String competencia) {
+        this.competencia.add(competencia);
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getEstado(){
+        return this.estado;
+    }
+
+    public void setEstado(String estado){
+        this.estado = estado;
+    }
+
+    public boolean cadastrar(){
+        String[] funcoes = {"nome","email","descricao","estado","token"};
+        String[] valores = {this.nome,Login.getLogin(),this.descricao,this.estado,Login.getToken()};
+        String jsonRecebido;
+        json = new ToJson("cadastrarVaga", funcoes, valores);
+        json.adicionarJson("faixaSalarial", this.faixaSalarial);
+        json.adicionarJson("competencias", this.competencia);
+        jsonRecebido = ClientApplication.enviarSocket(json.getJson());
+        json.setJson(new JSONObject(jsonRecebido));
+        if((json.getFuncao("status")+"").equals("201")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void buscar(){
+        String[] funcoes = {"email","token"};
+        String[] valores = {Login.getLogin(),Login.getToken()};
+        String jsonRecebido;
+        json = new ToJson("visualizarVaga", funcoes, valores);
+        json.adicionarJson("idVaga", this.codigo);
+        jsonRecebido = ClientApplication.enviarSocket(json.getJson());
+        json.setJson(new JSONObject(jsonRecebido));
+        this.descricao = json.getFuncao("descricao")+"";
+        this.faixaSalarial = Float.parseFloat(json.getFuncao("faixaSalarial")+"");
+        this.estado = json.getFuncao("estado")+"";
+        for(int i=0; i<((JSONArray) json.getFuncao("competencias")).length(); i++){
+            competencia.add(((JSONArray) json.getFuncao("competencias")).getString(i));
+        }
+    }
+
+    public boolean excluir(){
+        String[] funcoes = {"email","token"};
+        String[] valores = {Login.getLogin(),Login.getToken()};
+        String jsonRecebido;
+        json = new ToJson("apagarVaga", funcoes, valores);
+        json.adicionarJson("idVaga", codigo);
+        jsonRecebido = ClientApplication.enviarSocket(json.getJson());
+        json.setJson(new JSONObject(jsonRecebido));
+        if((json.getFuncao("status")+"").equals("201")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean atualizar(){
+        String[] funcoes = {"nome","email","descricao","estado","token"};
+        String[] valores = {this.nome,Login.getLogin(),this.descricao,this.estado,Login.getToken()};
+        String jsonRecebido;
+        json = new ToJson("atualizarVaga", funcoes, valores);
+        json.adicionarJson("faixaSalarial", this.faixaSalarial);
+        json.adicionarJson("competencias", this.competencia);
+        json.adicionarJson("idVaga", codigo);
+        jsonRecebido = ClientApplication.enviarSocket(json.getJson());
+        json.setJson(new JSONObject(jsonRecebido));
+        if((json.getFuncao("status")+"").equals("201")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Vaga [codigo=" + codigo + ", nome=" + nome + ", faixaSalarial=" + faixaSalarial + ", descricao="
+                + descricao + ", competencia=" + competencia + ", estado=" + estado + "]";
+    }
 }
